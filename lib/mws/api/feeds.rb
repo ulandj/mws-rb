a8ab02baa3335ae016105a56e23c1c06d601bf2e
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2020-02-05 13:34:03 +0600

    Add ability to upload Amazon Vat Invoices

--- lib/mws/api/feeds.rb
+++ lib/mws/api/feeds.rb
@@ -16,14 +16,31 @@ module MWS
 
       def submit_feed(params = {})
         xml_envelope = Envelope.new(params.merge!(merchant_id: connection.seller_id))
-        params = params.except(:merchant_id, :message_type, :message, :messages, :skip_schema_validation)
-        call(:submit_feed, params.merge!(
-                             request_params: {
-                               format: :xml,
-                               headers: { 'Content-MD5' => xml_envelope.md5, 'Content-Type' => 'text/xml' },
-                               body: xml_envelope.to_s
-                             }
-        ))
+        params = params.except(*params_to_except(params))
+        call(:submit_feed, params_to_request(params, xml_envelope))
+      end
+
+      protected
+
+      def params_to_except(params)
+        except_params = [:merchant_id, :message_type, :message, :messages, :skip_schema_validation]
+        except_params << :type if params[:message_type].to_s == 'invoice'
+        except_params
+      end
+
+      def params_to_request(params, envelope)
+        request_params = {
+          body: envelope.to_s,
+          headers: { 'Content-MD5' => envelope.md5, 'Content-Type' => 'text/xml' }
+        }
+
+        if params[:message_type].to_s == 'invoice'
+          request_params[:headers] = { 'Content-MD5' => envelope.md5, 'Content-Type' => 'application/octet-stream' }
+        else
+          request_params[:format] = :xml
+        end
+
+        params.merge!(request_params: request_params)
       end
     end
   end
